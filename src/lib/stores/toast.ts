3be7b6444d546d4020b2eb5846386bbb21977908
mcsdodo <lacny.jozef@gmail.com>
2026-04-27 16:03:19 +0200

    feat(toast): add withAction method for actionable toasts

--- src/lib/stores/toast.ts
+++ src/lib/stores/toast.ts
@@ -7,6 +7,7 @@ export interface Toast {
 	id: number;
 	message: string;
 	type: ToastType;
+	action?: { label: string; onClick: () => void };
 }
 
 function createToastStore() {
@@ -28,11 +29,39 @@ function createToastStore() {
 		update((toasts) => toasts.filter((t) => t.id !== id));
 	}
 
+	function withAction(
+		message: string,
+		actionLabel: string,
+		onAction: () => void,
+		duration = 6000
+	) {
+		const id = nextId++;
+		update((toasts) => [
+			...toasts,
+			{
+				id,
+				message,
+				type: TOAST_TYPES.INFO,
+				action: {
+					label: actionLabel,
+					onClick: () => {
+						onAction();
+						dismiss(id);
+					}
+				}
+			}
+		]);
+		if (duration > 0) {
+			setTimeout(() => dismiss(id), duration);
+		}
+	}
+
 	return {
 		subscribe,
 		success: (message: string) => show(message, TOAST_TYPES.SUCCESS),
 		error: (message: string) => show(message, TOAST_TYPES.ERROR, 6000),
 		info: (message: string) => show(message, TOAST_TYPES.INFO),
+		withAction,
 		dismiss
 	};
 }
